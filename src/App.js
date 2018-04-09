fe117079064686542d439d0ad7214c541f76d3e7
Daniel De Alday <danieldealday@gmail.com>
2018-04-09 15:24:15 -0700

    changed context api modularization

--- src/App.js
+++ src/App.js
@@ -3,16 +3,11 @@ import Provider from './Provider.js';
 import Family from './Family.js';
 
 class App extends Component {
-  state = {
-    name: 'Daniel',
-    energy: 9000,
-    cool: true
-  }
   render() {
     return (
       <Provider>
         <p>I'm the App</p>
-        <Family/>
+        <Family />
       </Provider>
     );
   }
