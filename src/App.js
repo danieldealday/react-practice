8b5c05527f8751a34077550a2da0fa03139de3e9
Daniel De Alday <danieldealday@gmail.com>
2018-04-16 11:12:26 -0700

    changed project folder structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component, Fragment } from 'react';
-import Provider from './Provider.js';
-import Family from './Family.js';
 
 class App extends Component {
   state = {
     name: 'Daniel',
-    energy: 9000,
+    age: 32,
     cool: true
   }
   render() {
     return (
-      <Provider>
-        <p>I'm the App</p>
-        <Family/>
-      </Provider>
+      <Fragment>
+        <p>Hello World</p>
+      </Fragment>
     );
   }
 }
