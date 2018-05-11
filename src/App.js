742cbb443eff71f85f4e36249c7813273fed97a8
Daniel De Alday <danieldealday@gmail.com>
2018-05-11 12:42:57 -0700

    event target handler for state update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { Component, Fragment } from 'react';
 
 class App extends Component {
+  state = {
+    text: 'text',
+    number: 1234,
+    bool: false
+  }
+  handleChange = (e) => {
+    let change = {};
+    console.log(e.target.name);
+    change[e.target.name] = e.target.value;
+    this.setState(change);
+  }
   render() {
     return (
       <Fragment>
-        <p>Hello World</p>
+        <form>
+          <label>
+            Full Name
+            <input type="text" name="text" onChange={this.handleChange} value={this.state.text} />
+          </label>
+
+          <label>
+            Home address
+            <input type="number" name="number" onChange={this.handleChange} value={this.state.number} />
+          </label>
+
+          <label>
+            Credit card number
+            <input type="text"  name="bool" onChange={this.handleChange} maxlength="16" value={this.state.bool} />
+          </label>
+        </form>
       </Fragment>
     );
   }
